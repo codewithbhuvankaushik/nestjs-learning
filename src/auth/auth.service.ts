9e9365ddd633855e6c56f700697846f1dc542e3f
Bhuvan Kaushik <codewithbhuvankaushik@gmail.com>
2026-05-22 23:45:02 +0530

    handle circular dependency between users and auth modules

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+    constructor(@Inject(forwardRef(() => UsersService)) private readonly userService: UsersService){}
+
+    isAuthenticated: boolean = false;
+
+    login(email: string, password: string) {
+        const user = this.userService.users.find(user => user.email == email && user.password == password);
+        if (user) {
+            this.isAuthenticated = true;
+
+            return "MY_TOKEN";
+        }
+
+        return "User doesn't exist";
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { forwardRef, Inject, Injectable } from "@nestjs/common";
+import { AuthService } from "src/auth/auth.service";
 
 @Injectable()
 export class UsersService {
-    users: {id: number, name: string, email: string, age: number, gender: string, isMarried: boolean}[] = [
-        {id: 1, name: "john", email: "john@gmail.com", age: 28, gender: "male", isMarried: true},
-        {id: 2, name: "mark", email: "mark@gmail.com", age: 32, gender: "male", isMarried: false}
+    constructor(@Inject(forwardRef(() => AuthService)) private readonly authService: AuthService) {}
+
+    users: {id: number, name: string, email: string, age: number, gender: string, isMarried: boolean, password: string}[] = [
+        {id: 1, name: "john", email: "john@gmail.com", age: 28, gender: "male", isMarried: true, password: "test123"},
+        {id: 2, name: "mark", email: "mark@gmail.com", age: 32, gender: "male", isMarried: false, password: "test234"}
     ]
 
     getAllUsers() {
-        return this.users;
+        if (this.authService.isAuthenticated) {
+            return this.users;
+        }
+
+        return [];
     }
 
     getUserById(id: number) {
         return this.users.find(user => user.id == id);
     }
 
-    createUser(user: {id: number, name: string, email: string, age: number, gender: string, isMarried: boolean}) {
+    createUser(user: {id: number, name: string, email: string, age: number, gender: string, isMarried: boolean, password: string}) {
         this.users.push(user);
     }
 }
