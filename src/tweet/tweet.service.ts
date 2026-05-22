579df85c289665eb8a764f64dbeaf0bfe53447c4
Bhuvan Kaushik <codewithbhuvankaushik@gmail.com>
2026-05-22 23:26:10 +0530

    establish inter modular dependency

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class TweetService {
+    constructor(private readonly userService: UsersService) {
+
+    }
     tweets: { text: string, date: Date, userId: number}[] = [
         { text: "First Tweet", date: new Date("2026-01-03"), userId: 1 },
         { text: "Second Tweet", date: new Date("2026-02-07"), userId: 1 },
@@ -11,8 +15,16 @@ export class TweetService {
     ];
 
     getTweets(userId?: number) {
-        if (userId)
-            return this.tweets.filter(tweet => tweet.userId == userId);
+        if (userId) {
+            const user = this.userService.getUserById(userId);
+            const tweets = this.tweets.filter(tweet => tweet.userId == userId);
+            
+            if (user) {
+                return tweets.map(tweet => ({text: tweet.text, date: tweet.date, name: user.name}));
+            }
+
+            return tweets;
+        }
 
         return this.tweets;
     }
