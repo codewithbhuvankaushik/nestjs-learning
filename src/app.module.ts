32dbfa3179ad93d2725afc4c88d4be2bedce33db
Bhuvan Kaushik <codewithbhuvankaushik@gmail.com>
2026-05-23 15:11:28 +0530

    establish connection with postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,7 +7,16 @@ import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [UsersModule, TweetModule, AuthModule, TypeOrmModule.forRoot({
+    type: "postgres",
+    entities: [],
+    synchronize: true,
+    host: "localhost",
+    port: 5433,
+    username: "admin",
+    password: "test",
+    database: "appdb"
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
